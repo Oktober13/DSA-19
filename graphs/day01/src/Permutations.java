df28e951156aecf54350cad8e245c727fdc2b3e3
lzuehsow <lydiazuehsow@gmail.com>
2019-04-04 22:03:31 -0400

    Pushing permutations

--- graphs/day01/src/Permutations.java
+++ graphs/day01/src/Permutations.java
@@ -1,13 +1,33 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Permutations {
-
     public static List<List<Integer>> permutations(List<Integer> A) {
-        // TODO
-        List<List<Integer>> permutations = new LinkedList<>();
-        return permutations;
+        List<List<Integer>> returnVal = new LinkedList<>();
+        int num = A.size();
+
+        Permutations.permute(returnVal, A, 0, num-1);
+        return returnVal;
+    }
+
+    private static void permute(List<List<Integer>> p, List<Integer> mat, int l, int r) {
+        if (l == r) { // If lower range == higher range (number has been filled)
+            p.add(new LinkedList<>(mat));
+        } else {
+            for (int i = l; i <= r; i++) { // Iterate through range
+                mat = swap(mat,l,i); // Reorder number ABC -> BAC
+                permute(p, mat, l+1, r); // Recurse, move window one number right.
+                mat = swap(mat,l,i); // Reorder number ABC -> BAC
+            }
+        }
+    }
+
+    public static List<Integer> swap(List<Integer> A, int i, int j)
+    {
+        Integer temp;
+        temp = A.get(i) ;
+        A.set(i, A.get(j));
+        A.set(j, temp);
+        return A;
     }
 
 }

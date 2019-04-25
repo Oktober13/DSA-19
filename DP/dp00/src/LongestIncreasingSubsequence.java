ad9dd2d7be4290a2fdb2cb5e673f577f9ec45666
lzuehsow <lydiazuehsow@gmail.com>
2019-04-25 17:18:32 -0400

    Pushing working Longest Increasing Subsequence

--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,34 @@
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
 
     // Runtime: TODO
     // Space: TODO
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        int[] sequence = new int[A.length];
+        int max = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            max = -1; // Maximum length of subsequence
+            for (int j = 0; j < i; j++) {
+                if(A[i] > A[j]) {
+                    if ((max == -1) || (max < sequence[j] + 1)) {
+                        max = 1 + sequence[j];
+                    }
+                }
+            }
+
+            if (max == -1) { // No prev elements were smaller than sequence[i]
+                max = 1;
+            }
+            sequence[i] = max;
+        }
+
+        Arrays.sort(sequence);
+
+        if (sequence.length == 0) {
+            return 0;
+        }
+        return sequence[sequence.length - 1];
     }
 }

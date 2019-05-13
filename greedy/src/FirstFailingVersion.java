ff3c06bf35db35118e82b8ddf66f777507199580
lzuehsow <lydiazuehsow@gmail.com>
2019-05-13 09:14:41 -0400

    Pushing binary search

--- greedy/src/FirstFailingVersion.java
+++ greedy/src/FirstFailingVersion.java
@@ -1,7 +1,26 @@
 public class FirstFailingVersion {
 
+    //Runtime: N log(N)
     public static long firstBadVersion(long n, IsFailingVersion isBadVersion) {
-        // TODO
-        return -1;
+        // Binary search
+
+        long r = n;
+        long l = 1;
+
+        // While left and right window indices do not overlap
+        while (l < r) {
+            // Find middle index
+            long mid = l + (r - l) / 2;
+
+            // Divide in half
+            if (isBadVersion.isFailingVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        // Return overlapped index
+        return l;
     }
 }

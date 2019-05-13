db2437f012373435823fa24eb6d0605d736d08f8
lzuehsow <lydiazuehsow@gmail.com>
2019-05-13 00:53:17 -0400

    Pushing working Dungeon Game!

--- DP/day01/src/DungeonGame.java
+++ DP/day01/src/DungeonGame.java
@@ -1,42 +1,27 @@
-import java.util.Collections;
-import java.util.HashMap;
-
 public class DungeonGame {
 
     public static int minInitialHealth(int[][] map) {
         int[][] health = new int[map.length][map[0].length];
-        health[0][0] = map[0][0];
-        return findPath(map, health,map.length-1, map[0].length-1);
+        health[map.length-1][map[0].length-1] = Math.max((1 - map[map.length-1][map[0].length-1]), 1);
+        health = findPath(map, health);
+
+        return health[0][0];
     }
 
-    private static int findPath(int[][] map, int[][] health, int i, int j) {
-        if (health[i][j] == map[i][j] + 1) {
-            return health[i][j];
+    private static int[][] findPath(int[][] map, int[][] health) {
+        for (int i = map.length-2; i >= 0; i--) { // Initialize last column
+            health[i][map[0].length-1] = Math.max(health[i+1][map[0].length-1] - map[i][map[0].length-1], 1);
+        }
+        for (int j = map[0].length-2; j >= 0; j--) { // Initialize first row
+            health[map.length-1][j] = Math.max(health[map.length-1][j+1] - map[map.length-1][j], 1);
         }
 
-        HashMap<Integer, int[]> neighbors = new HashMap<>();
-
-        for (int k = -1; k <= 1; k++) {
-            for (int l = -1; l <= 1 ; l++) {
-                if (!(k == 0 && l == 0) && (inBoard(i + k, j + l, map.length, map[0].length))) {
-                    int[] coord = new int[]{i + k, j + l};
-                    neighbors.put(health[i + k][j + l], coord);
-                }
+        // Recursively populate health
+        for (int i = map.length-2; i >= 0; i--) {
+            for (int j = map[0].length-2; j >= 0; j--) {
+                health[i][j] = Math.min(Math.max(health[i+1][j] - map[i][j], 1), Math.max(health[i][j+1] - map[i][j], 1));
             }
         }
-
-        Integer min = Collections.max(neighbors.keySet());
-        int newI = neighbors.get(min)[0];
-        int newJ = neighbors.get(min)[1];
-
-        health[i][j] = findPath(map, health, newI, newJ) + map[i][j];
-        return health[i][j];
-    }
-
-    private static boolean inBoard(int i, int j, int length, int width) {
-        if (i >= 0 && i < length && j >= 0 && j < width) {
-            return true;
-        }
-        return false;
+        return health;
     }
 }
